f80cf1f381bc765e2f74ad3a7a8076289a1d11e1
Richard Vade <ivmidable@gmail.com>
2022-11-16 14:34:27 -0500

    completed version of challenge

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -67,33 +67,80 @@ mod tests {
             .unwrap()
     }
 
+    fn get_message_from_owner(app: &App, messages_contract: &MessagesContract, owner:Addr) -> MessagesResponse {
+        app.wrap()
+            .query_wasm_smart(messages_contract.addr(), &QueryMsg::GetMessagesByAddr { address: owner.to_string() })
+            .unwrap()
+    }
+
+    fn get_message_by_topic(app: &App, messages_contract: &MessagesContract, topic:String) -> MessagesResponse {
+        app.wrap()
+            .query_wasm_smart(messages_contract.addr(), &QueryMsg::GetMessagesByTopic { topic: topic })
+            .unwrap()
+    }
+
+    fn get_message_by_id(app: &App, messages_contract: &MessagesContract, id:Uint128) -> MessagesResponse {
+        app.wrap()
+            .query_wasm_smart(messages_contract.addr(), &QueryMsg::GetMessagesById { id: id })
+            .unwrap()
+    }
+
     fn get_balance(app: &App, user:String, denom:String) -> Coin {
         app.wrap().query_balance(user, denom).unwrap()
     }
 
     fn add_message(app: &mut App, messages_contract: &MessagesContract, owner:Addr, topic:String, message:String) {
-        //use ExecuteMsg to add a message
-        //use app.execute_contract to send message to contract
+        let msg = ExecuteMsg::AddMessage {
+            topic: topic,
+            message: message
+        };
+        app.execute_contract(
+            owner,
+            messages_contract.addr(),
+            &msg,
+            &[],
+        )
+        .unwrap();
     }
 
     #[test]
     fn add_two_messages_and_query_all_messages() {
-
+        let (mut app, messages_id) = store_code();
+        let messages_contract = messages_contract(&mut app, messages_id);
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER1), "topic1".to_string(), "message1".to_string());
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER1), "topic1".to_string(), "message2".to_string());
+        let messages = get_all_messages(&app, &messages_contract);
+        assert_eq!(messages.messages.len(), 2);
     }
 
     #[test]
     fn add_messages_from_two_owners_and_query_messages_by_owner() {
-
+        let (mut app, messages_id) = store_code();
+        let messages_contract = messages_contract(&mut app, messages_id);
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER1), "topic1".to_string(), "message1".to_string());
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER2), "topic1".to_string(), "message2".to_string());
+        let messages = get_message_from_owner(&app, &messages_contract, Addr::unchecked(USER1));
+        assert_eq!(messages.messages.len(), 1);
     }
 
     #[test]
     fn add_messages_from_two_owners_and_query_messages_by_topic() {
-
+        let (mut app, messages_id) = store_code();
+        let messages_contract = messages_contract(&mut app, messages_id);
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER1), "topic1".to_string(), "message1".to_string());
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER2), "topic1".to_string(), "message2".to_string());
+        let messages = get_message_by_topic(&app, &messages_contract, "topic1".to_string());
+        assert_eq!(messages.messages.len(), 2);
     }
 
     #[test]
     fn add_two_messages_and_query_messages_by_id() {
-    
+        let (mut app, messages_id) = store_code();
+        let messages_contract = messages_contract(&mut app, messages_id);
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER1), "topic1".to_string(), "message1".to_string());
+        add_message(&mut app, &messages_contract, Addr::unchecked(USER2), "topic1".to_string(), "message2".to_string());
+        let messages = get_message_by_id(&app, &messages_contract, Uint128::new(1));
+        assert_eq!(messages.messages.len(), 1);
     }
 
 }
